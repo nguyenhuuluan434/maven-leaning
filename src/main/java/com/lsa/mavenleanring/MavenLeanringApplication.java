264868e1115fd7c800a627dd7c2089d4c7e61968
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2019-08-29 19:31:23 +0700

    init project

---
+++ src/main/java/com/lsa/mavenleanring/MavenLeanringApplication.java
@@ -0,0 +1,13 @@
+package com.lsa.mavenleanring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenLeanringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenLeanringApplication.class, args);
+	}
+
+}
